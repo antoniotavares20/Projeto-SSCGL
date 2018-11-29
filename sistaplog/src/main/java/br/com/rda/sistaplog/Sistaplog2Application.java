6a616833c74ef2f9bffac998e0fd74239e506dfa
Antonio Tavares <antoniotavarespereira@gmail.com>
2018-11-29 13:06:11 -0200

    adicionou template

---
+++ sistaplog/src/main/java/br/com/rda/sistaplog/Sistaplog2Application.java
@@ -0,0 +1,12 @@
+package br.com.rda.sistaplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sistaplog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sistaplog2Application.class, args);
+	}
+}
